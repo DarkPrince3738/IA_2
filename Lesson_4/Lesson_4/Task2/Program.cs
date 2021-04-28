a5ed43d1b2b70fbc3152243af70ae2d5716d5865
Alex <darkprince3738@gmail.com>
2021-04-28 20:13:14 +0300

    All Done

---
+++ Lesson_4/Lesson_4/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the Earth from the sun behind the number?");
+            string answer = Console.ReadLine();
+            if((answer == "Third" || answer == "third" || answer == "THIRD") && answer !="")
+                    { 
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+        }
+    }
+}
---
+++ Lesson_4/Lesson_4/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the number from 1-100");
+            int answer = int.Parse(Console.ReadLine());
+            if((answer >= 1 && answer<=10) || (answer >= 15 && answer <= 25) || (answer >= 30 && answer <= 70) || (answer >= 75 && answer <= 98) || (answer == 100))
+            {
+                Console.WriteLine("Got You");
+            }
+            else
+            {
+                Console.WriteLine("You're in safe");
+            }
+        }
+    }
+}
